fb43433133a21b8e72d4c16e78a10a08e963a47e
nehadalvi1010@gmail.com <nehadalvi1010@gmail.com>
2020-08-03 11:25:19 +0530

    vote

---
+++ app1/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from app1.models import Question,Choice
+
+# Register your models here.
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
